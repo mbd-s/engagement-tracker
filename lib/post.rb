22b4515d7058134f43e2fe6f768d7fe405970328
mbd-s <michaelbsilverberg@gmail.com>
2017-01-08 18:09:03 +0100

    lightens initialize for post

--- lib/post.rb
+++ lib/post.rb
@@ -3,17 +3,16 @@ class Post
   def initialize(node)
     @page_id, @id = node['id'].split('_')
     @post_type = node['type']
-    @reactions = extract_reactions node
-    @comments = extract_comments node
+    @node = node
   end
 
-  def to_csv
-    @reactions.map { |r| r.to_csv } + @comments.map { |c| c.to_csv }
+  def csv_lines
+    reactions(@node).map { |r| r.to_csv } + comments(@node).map { |c| c.to_csv }
   end
 
   private
 
-  def extract_reactions(node)
+  def reactions(node)
     if node['reactions']
       node['reactions']['data'].map { |r| Reaction.new(@page_id, @id, @post_type, r) }
     else
@@ -21,7 +20,7 @@ class Post
     end
   end
 
-  def extract_comments(node)
+  def comments(node)
     if node['comments']
       node['comments']['data'].map { |c| Comment.new(@page_id, @id, @post_type, c)}
     else
