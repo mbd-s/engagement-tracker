4bfbc161471874b5286890f176ead3a7f290366a
mbd-s <michaelbsilverberg@gmail.com>
2017-01-09 14:15:36 +0100

    changes params of fb get request to include commenter id

--- lib/client.rb
+++ lib/client.rb
@@ -10,9 +10,8 @@ class Client
     @page_id = page_id
   end
 
-  #TODO refactor: use HTTParty to accept array of params and interpolate into url
   def get_posts(page_id, limit)
-    response = Client.get("/#{page_id}?fields=posts.limit(#{limit})%7Btype%2Creactions%7Bid%2Ctype%7D%2Ccomments%7Bid%7D%7D")
+    response = Client.get("/#{page_id}?fields=posts.limit(#{limit})%7Btype%2Creactions%7Btype%7D%2Ccomments%7Bfrom%7D%7D")
     if response.success?
       response['posts']['data']
     else
