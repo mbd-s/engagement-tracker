ebc70b2baa8280a2424ddf29b4540a09a2c7f02e
mbd-s <michaelbsilverberg@gmail.com>
2017-01-09 14:16:37 +0100

    updates commenter id from placeholder text to the correct json path

--- lib/comment.rb
+++ lib/comment.rb
@@ -3,10 +3,10 @@ class Comment
     @page_id = page_id
     @post_id = post_id
     @post_type = post_type
-    @user_id = "user_idTK"
+    @user_id = node['from']['id']
   end
 
   def to_csv
-    [@user_id, @page_id, @post_id, @post_type, "comment"]
+    [@user_id, @page_id, @post_id, @post_type, 'comment']
   end
 end
