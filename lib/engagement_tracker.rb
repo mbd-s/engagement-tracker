fcba617ee762226c069d745547a3425bad4d7be7
mbd-s <michaelbsilverberg@gmail.com>
2017-01-10 15:24:57 +0100

    removes to-dos

--- lib/engagement_tracker.rb
+++ lib/engagement_tracker.rb
@@ -6,11 +6,6 @@ Dotenv.load
 
 require_relative 'engagement_tracker/post_processor'
 
-#TODO pagination
-#TODO tests
-#TODO refactor: use HTTParty to accept array of params and interpolate into url
-#TODO get page name and id from fb response
-
 begin
   CSV.foreach('../input.csv') do |row|
     id, limit = row
